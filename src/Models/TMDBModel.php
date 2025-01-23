1db219cb9f54d8546509b92e056bb678a7d1a66e
Joseph Padfield <josephpadfield@mac.home>
2025-01-23 19:04:08 +0000

    Retrieving more detailed information on a film.

--- src/Models/TMDBModel.php
+++ src/Models/TMDBModel.php
@@ -31,6 +31,20 @@ class TMDBModel
             ],
         ]);
 
-        return $response->getBody();
+        $response2 = $client->request('GET', 'https://api.themoviedb.org/3/movie/' . $id . '/credits', [
+            'headers' => [
+                'Authorization' => 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1OGEwYWI0MjYxODZkNjgxMDY5YTdkOTIxZmFiZWU3ZSIsIm5iZiI6MTczMzEzNjU2My4yMDQsInN1YiI6IjY3NGQ5MGIzZTdkNmY4MjkyZTBiOGZjNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dWzkHKvEIR4192_L3d10rXcnIImGs0h2_0NoFjfddyI',
+                'accept' => 'application/json',
+            ]
+        ]);
+
+        $responseData= json_decode($response->getBody(), true);
+        $response2Data= json_decode($response2->getBody(), true);
+
+        $combinedResponseData = [
+            $responseData, $response2Data,
+        ];
+
+        return json_encode($combinedResponseData);
     }
 }
