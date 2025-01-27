ebc9906ebc786c37e05bc19d970484619eed1208
Joseph Padfield <josephpadfield@mac.home>
2025-01-27 18:35:29 +0000

    search functionality wired up

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,3 +1,16 @@
-document.getElementById('add-film-button').addEventListener('click', function(){
+const addFilmButton = document.getElementById('add-film-button')
+const searchFilmsSubmit = document.getElementById('search-films-submit')
+const searchFilmsInput = document.getElementById('search-films-input')
+
+addFilmButton.addEventListener('click', function(){
     console.log('clicked');
 })
+
+searchFilmsSubmit.addEventListener('click', async function(event){
+    event.preventDefault()
+    console.log(searchFilmsInput.value)
+    let response = await fetch(`http://0.0.0.0:8080/searchFilm/` + searchFilmsInput.value)
+    let result = await response.json()
+    console.log(result)
+})
+
