db6b7d61b3b258d9b4a04eaad045f997e27aacd6
Joseph Padfield <josephpadfield@mac.home>
2025-01-28 11:11:16 +0000

    basic homepage plan

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,16 +1,24 @@
-const addFilmButton = document.getElementById('add-film-button')
-const searchFilmsSubmit = document.getElementById('search-films-submit')
-const searchFilmsInput = document.getElementById('search-films-input')
-
-addFilmButton.addEventListener('click', function(){
-    console.log('clicked');
-})
-
-searchFilmsSubmit.addEventListener('click', async function(event){
-    event.preventDefault()
-    console.log(searchFilmsInput.value)
-    let response = await fetch(`http://0.0.0.0:8080/searchFilm/` + searchFilmsInput.value)
-    let result = await response.json()
-    console.log(result)
-})
-
+// const addFilmButton = document.getElementById('add-film-button')
+// const searchFilmsSubmit = document.getElementById('search-films-submit')
+// const searchFilmsInput = document.getElementById('search-films-input')
+// const searchResultsContainer = document.getElementById('search-results-container')
+//
+//
+//
+// addFilmButton.addEventListener('click', function(){
+//     console.log('clicked');
+// })
+//
+// searchFilmsSubmit.addEventListener('click', async function(event){
+//     event.preventDefault()
+//     let response = await fetch(`http://0.0.0.0:8080/searchFilm/` + searchFilmsInput.value)
+//     let searchResults = await response.json()
+//     searchResults.results.forEach((result) => {
+//         const posterImage = document.createElement("img")
+//         posterImage.src = 'https://image.tmdb.org/t/p/w500' + result.poster_path
+//         posterImage.alt = result.original_title
+//         posterImage.classList.add('poster-image')
+//         searchResultsContainer.append(posterImage)
+//     })
+// })
+//
