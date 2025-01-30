d752754e062e2111ef6e3c0e713698010675973b
Joseph Padfield <josephpadfield@mac.home>
2025-01-30 20:38:54 +0000

    minor changes to DeleteFilmController

--- src/Controllers/DeleteFilmController.php
+++ src/Controllers/DeleteFilmController.php
@@ -2,27 +2,29 @@
 
 namespace App\Controllers;
 
-use App\Models\FilmsModel;
+use App\Interfaces\FilmsModelInterface;
 use App\Abstracts\Controller;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 class DeleteFilmController extends Controller
 {
-    private FilmsModel $model;
+    private FilmsModelInterface $model;
 
-    public function __construct(FilmsModel $model)
+    public function __construct(FilmsModelInterface $model)
     {
         $this->model = $model;
     }
 
     public function __invoke(Request $request, Response $response, array $args): Response
     {
+        $error = null;
+
         if (!isset($args['id']))
         {
             $error = 'Film id not provided';
         }
-        elseif (!is_int($args['id']))
+        elseif (!is_numeric($args['id']))
         {
             $error = 'Film id is not an integer';
         }
