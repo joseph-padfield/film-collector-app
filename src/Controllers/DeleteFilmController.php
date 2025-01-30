b76cb93ac0a1efbac9c7cefd9953c6fad3a97396
Joseph Padfield <josephpadfield@mac.home>
2025-01-30 20:22:05 +0000

    refined DeleteFilmController and validation

--- src/Controllers/DeleteFilmController.php
+++ src/Controllers/DeleteFilmController.php
@@ -14,11 +14,26 @@ class DeleteFilmController extends Controller
     public function __construct(FilmsModel $model)
     {
         $this->model = $model;
-        $this->renderer = $renderer;
     }
 
     public function __invoke(Request $request, Response $response, array $args): Response
     {
+        if (!isset($args['id']))
+        {
+            $error = 'Film id not provided';
+        }
+        elseif (!is_int($args['id']))
+        {
+            $error = 'Film id is not an integer';
+        }
+        if($error)
+        {
+            return $this->respondWithJson($response, ([
+                'status' => 'error',
+                'message' => 'Validation failed',
+                'errors' => ['id' => $error]
+            ]));
+        }
         try {
             $id = $this->model->deleteFilm($args['id']);
 
@@ -26,7 +41,7 @@ class DeleteFilmController extends Controller
             {
                 return $this->respondWithJson($response, ['message' => 'Film not found.'], 404);
             }
-            return $response->withHeader('Content-Type', '/films')->withStatus(200);
+            return $this->respondWithJson($response, ['message' => 'Film successfully deleted.'], 200);
         } catch (\PDOException $e) {
             return $this->respondWithJson($response, ['error' => $e->getMessage()], 500);
         }
